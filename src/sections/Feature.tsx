334e0710d50d1fcbf5477346128d64c3987d650b
R3sty <iral.resty@gmail.com>
2023-03-28 16:57:38 +0900

    finished feature section

--- src/sections/Feature.tsx
+++ src/sections/Feature.tsx
@@ -1,14 +1,31 @@
+import image1 from '../assets/desktop/image-women-videochatting.jpg';
+import image2 from '../assets/desktop/image-woman-in-videocall.jpg';
+import image3 from '../assets/desktop/image-men-in-meeting.jpg';
+import image4 from '../assets/desktop/image-women-videochatting.jpg';
+
+const images = [image1, image2, image3, image4];
+
 const Feature = () => {
 	return (
-		<div className='feature-container'>
-			<h1>Feature Section works</h1>
-			<h2>
-				01 Built for modern use Smarter meetings, all in one place Send
-				messages, share files, show your screen, and record your meetings — all
-				in one workspace. Control who can join with invite-only team access,
+		<main className='feature-container'>
+			<div className='__img-container'>
+				{images.map((img, index) => (
+					<img key={index} src={img} alt={`Feature ${index + 1}`}></img>
+				))}
+			</div>
+			<h2>BUILT FOR MODERN USE</h2>
+			<h1>Smarter meetings, all in one place</h1>
+			<p>
+				Send messages, share files, show your screen, and record your meetings —
+				all in one workspace. Control who can join with invite-only team access,
 				data encryption, and data export.
-			</h2>
-		</div>
+			</p>
+			<div className='__divider-container'>
+				<div className='__divider-container __divider'>
+					<span className='__divider-container __divider-number'>02</span>
+				</div>
+			</div>
+		</main>
 	);
 };
 
