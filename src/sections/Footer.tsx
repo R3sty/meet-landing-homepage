d00c881530b399a571685dfeff282b8271a3f5cf
R3sty <iral.resty@gmail.com>
2023-03-29 13:36:51 +0900

    finished mobile layout

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,28 +2,32 @@ import Button from '../components/Button';
 
 const Footer = () => {
 	return (
-		<div className='footer-container'>
+		<>
 			<div className='__divider-container'>
 				<div className='__divider-container __divider'>
 					<span className='__divider-container __divider-number'>02</span>
 				</div>
 			</div>
-			<div className='__footer-image'></div>
-			<h1>Experience more together</h1>
-			<h2>
-				Stay connected with reliable HD meetings and unlimited one-on-one and
-				group video sessions.
-			</h2>
-			<Button
-				type={'submit'}
-				buttonText={
-					<span>
-						Download <span className='__version-text'> v1.3</span>
-					</span>
-				}
-				className='button'
-			></Button>
-		</div>
+			<div className='footer-container'>
+				<div className='__footer-content'>
+					<h1>Experience more together</h1>
+					<p>
+						Stay connected with reliable HD meetings and unlimited one-on-one
+						and group video sessions.
+					</p>
+					<Button
+						type={'submit'}
+						buttonText={
+							<span>
+								Download <span className='__version-text'> v1.3</span>
+							</span>
+						}
+						className='button __secondary'
+					></Button>
+				</div>
+				<div className='__footer-image'></div>
+			</div>
+		</>
 	);
 };
 
